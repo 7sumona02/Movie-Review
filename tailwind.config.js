ebb98b48726aeccfbf00b3e5036a3ef33f6b95fe
7sumona02 <145152897+7sumona02@users.noreply.github.com>
2023-12-24 12:31:05 +0530

    added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {'cormorant': ['Cormorant','serif'],
+  },
+    extend: {},
+  },
+  plugins: [],
+}
+
